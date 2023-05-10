08c7ba61089be9db65899b19fad108439cdd9721
Alex Bustos <bustosalex1@gmail.com>
2023-05-09 20:03:31 -0400

    not that I would want to keep this, but just in case this is the completed astro blog tutorial

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let messages: string[];
+    const randomMessage = () =>
+        messages[Math.floor(Math.random() * messages.length)];
+
+    let greeting = messages[0];
+</script>
+
+<div>
+    <h3>{greeting}! Thank you for visiting!</h3>
+    <button on:click={() => (greeting = randomMessage())}>New Greeting</button>
+</div>
