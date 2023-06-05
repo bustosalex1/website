ffff754409bb28b2a960ee1f404b63951183379a
Alex Bustos <bustosalex1@gmail.com>
2023-06-05 05:16:33 -0400

    lol hoverable image captions are looking pretty much as I envisioned them... now I'm just wondering if they were a good idea or not...

--- src/components/ClickableImage.svelte
+++ src/components/ClickableImage.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
     import { cubicInOut } from "svelte/easing";
-    import { fade, fly } from "svelte/transition";
+    import { fade, fly, slide } from "svelte/transition";
     export let caption: string | undefined;
+    export let showCaption: boolean | undefined;
     let selected = false;
-
+    let hovered = false;
     const toggleModal = () => {
         selected = !selected;
     };
 </script>
 
 <!-- default image -->
-<button on:click={toggleModal} class="cursor-pointer">
+<button
+    on:click={toggleModal}
+    class="relative group cursor-pointer overflow-hidden rounded-md"
+    on:mouseenter={() => (hovered = true)}
+    on:mouseleave={() => (hovered = false)}
+>
+    {#if hovered}
+        {#if caption}
+            <div
+                class="absolute bottom-0 left-0 h-auto w-full bg-black bg-opacity-70 p-2 rounded-b-md text-white flex justify-start items-center text-left"
+                transition:fly={{ y: 50, duration: 250, easing: cubicInOut }}
+            >
+                {caption}
+            </div>
+        {/if}
+    {/if}
+
     <slot />
-    {#if caption}
+    {#if caption && showCaption}
         <em>{caption}</em>
     {/if}
 </button>
@@ -20,7 +37,7 @@
 {#if selected}
     <!-- modal container -->
     <button
-        class="fixed top-0 left-0 h-[100vh] w-[100vw] bg-black bg-opacity-50 flex items-center justify-center cursor-pointer"
+        class="fixed top-0 left-0 h-[100vh] w-[100vw] bg-black bg-opacity-70 flex items-center justify-center cursor-pointer z-50"
         on:click={toggleModal}
         transition:fade={{ duration: 250, easing: cubicInOut }}
     >
@@ -30,6 +47,9 @@
             transition:fly={{ duration: 250, y: 50, easing: cubicInOut }}
         >
             <slot />
+            {#if caption}
+                <em class="text-white">{caption}</em>
+            {/if}
         </div>
     </button>
 {/if}
