afe6f5fcb19d8571a2f1b0a307d2987842ce3384
Alex Bustos <bustosalex1@gmail.com>
2023-12-27 23:13:48 -0500

    search is starting to look deece

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,18 +1,50 @@
 <script lang="ts">
-    import type { CollectionEntry } from "astro:content";
-    import Fuse from "fuse.js";
-
+    import { type CollectionEntry } from "astro:content";
+    import Fuse, { type FuseResult } from "fuse.js";
+    import SearchMatch from "./SearchMatch.svelte";
     export let posts: CollectionEntry<"notes">[];
+    export let dropdown: boolean = false;
     export let keys: string[];
 
-    const fuse = new Fuse(posts, { keys: keys });
+    const fuse = new Fuse(posts, {
+        keys: keys,
+        findAllMatches: false,
+        includeMatches: true,
+        ignoreLocation: true,
+        shouldSort: true,
+    });
 
-    const results = [];
-    const onInput = (input: string) => {
-        return fuse.search(input);
-    };
+    let results: FuseResult<CollectionEntry<"notes">>[] = [];
+    let searchValue = "";
+
+    $: {
+        if (searchValue.length > 0) {
+            results = fuse.search(searchValue);
+        } else {
+            results = [];
+        }
+    }
 </script>
 
-<div>
-    <input type="text" placeholder="Search..." />
+<div class="w-full" class:dropdown>
+    <!-- search box -->
+    <input
+        type="text"
+        class="input input-bordered input-sm w-full"
+        placeholder="Search notes..."
+        bind:value={searchValue}
+        tabindex="0"
+    />
+    <!-- search results -->
+    <div
+        class="flex flex-col gap-2 w-full mt-4"
+        class:dropdown-content={dropdown}
+        class:z-[1]={dropdown}
+        tabindex="0"
+        role="button"
+    >
+        {#each results as result (result.item.id)}
+            <SearchMatch {result} />
+        {/each}
+    </div>
 </div>
---
+++ src/components/SearchMatch.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { type CollectionEntry } from "astro:content";
+    import { type FuseResult } from "fuse.js";
+    export let result: FuseResult<CollectionEntry<"notes">>;
+</script>
+
+<div
+    class="flex flex-col w-full gap-1 rounded-md ring-1 ring-neutral-400 p-2 bg-base-100 drop-shadow-md"
+>
+    <div class="flex flex-row justify-between items-center">
+        <a href={"/notes/" + result.item.slug} class="hoverable"
+            >{result.item.data.title}</a
+        >
+        <div class="text-xs font-mono">
+            {result.item.data.pubDate.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                timeZone: "UTC",
+            })}
+        </div>
+    </div>
+    <div class="text-xs">
+        {result.item.data.class}
+    </div>
+</div>
