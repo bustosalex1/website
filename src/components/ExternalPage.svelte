d9da1faae8ac1dd50417016eb47e007f6a828f11
Alex Bustos <bustosalex1@gmail.com>
2024-07-14 15:16:35 -0400

    adding externalpage component

---
+++ src/components/ExternalPage.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    /** The URL of the page to link to. */
+    export let link: string;
+
+    /** An optional title to display. if not specified, this defaults to "Project Page".*/
+    export let title = "Project Page";
+
+    let selected = false;
+</script>
+
+<!--@component
+Simple component that wraps an external page in an `iframe` and provides options to full-screen the
+page or go to the page. My intent with this is just to provide a way to embed other web projects on
+my website.
+-->
+<!--rounded rectangle that wraps the iframe and title.-->
+<div
+    class={`rounded-md ring-base-content/20 ring-1 bg-base-100 ${selected ? "fixed top-0 left-0 h-screen w-screen z-50" : "h-full overflow-clip"}`}
+>
+    <!--row providing a title and some buttons to full screen or go to the page-->
+    <div
+        class="flex flex-row justify-between border-b border-1 items-center p-2"
+    >
+        <div class="font-semibold">{title}</div>
+
+        <div class="flex flex-row space-x-1">
+            <button
+                class="btn btn-xs btn-ghost"
+                on:click={() => {
+                    selected = !selected;
+                }}>Go to Page</button
+            >
+            <button
+                class="btn btn-xs btn-ghost"
+                on:click={() => {
+                    selected = !selected;
+                }}>Full Screen</button
+            >
+        </div>
+    </div>
+    <iframe
+        src={link}
+        class={`w-full min-h-fit max-h-none ${selected ? "h-full" : "h-[75vh]"}`}
+        {title}
+    ></iframe>
+</div>
