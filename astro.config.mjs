ea250c7d210c6e92643b5200ba565a624e9d36e7
Alex Bustos <bustosalex1@gmail.com>
2024-07-18 15:42:39 -0400

    switching from strings to imports in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [svelte(), tailwind(), mdx()],
     site: "https://bustosalex1.github.io",
     markdown: {
-        remarkPlugins: ["remark-math"],
+        remarkPlugins: [remarkMath],
         rehypePlugins: [
             [
-                "rehype-katex",
+                rehypeKatex,
                 {
                     // katex plugin options
                 },
