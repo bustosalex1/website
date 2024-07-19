ded46a80105da1ce078152fbc7897d54cd037b71
Alex Bustos <bustosalex1@gmail.com>
2024-07-18 21:57:28 -0400

    removing more iconify/tailwind stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,10 @@
-const { addDynamicIconSelectors } = require("@iconify/tailwind");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {},
     },
-    plugins: [
-        require("daisyui"),
-        require("@tailwindcss/typography"),
-        addDynamicIconSelectors(),
-    ],
+    plugins: [require("daisyui"), require("@tailwindcss/typography")],
 
     theme: {
         extend: {
